04b0cd836571c9f5561beeed5f9b586c4c942212
Tabare Garcia Piniñor <tabareg14@gmail.com>
2020-01-08 16:36:07 -0300

    Add tracker_handler to handle GET request and response to tracker

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use std::io;
 use std::path::Path;
 use std::error::Error;
+use rand::Rng;
+use rand::distributions::Alphanumeric;
 use crate::torrent_handler::Torrent;
 
 mod torrent_handler;
+mod tracker_handler;
 
 fn main() {
     let input = read_input().unwrap();
     let path = Path::new(&input);
     let torrent = Torrent::open(path).unwrap();
+    let peer_id = rand::thread_rng().sample_iter(Alphanumeric).take(20).collect();
+    let port = 6881;
+    let res = tracker_handler::request_peers(torrent, &peer_id, &port).unwrap();
 
-    println!("{:?}", torrent);
+    println!("{:#?}", res);
 }
 
 fn read_input() -> Result<String, Box<dyn Error>> {
---
+++ src/tracker_handler.rs
@@ -0,0 +1,83 @@
+use crate::torrent_handler::Torrent;
+use reqwest;
+use reqwest::Url;
+use std::time::Duration;
+use std::error::Error;
+use percent_encoding::percent_encode_byte;
+use std::net::Ipv4Addr;
+use serde::{Deserialize, Deserializer, de};
+use serde::de::Visitor;
+use core::fmt;
+use byteorder::{BigEndian, ByteOrder};
+
+struct PeerVecVisitor;
+#[derive(Debug, Deserialize)]
+struct Peer {
+    ip: Ipv4Addr,
+    port: u16
+}
+#[derive(Debug, Deserialize)]
+pub struct TrackerResponse {
+    interval: u32,
+    #[serde(deserialize_with = "Peer::vec_from_bytes")]
+    peers: Vec<Peer>
+}
+
+impl Peer {
+    fn from_bytes(b: &[u8]) -> Peer {
+        let ip = Ipv4Addr::new(b[0], b[1], b[2], b[3]);
+//        let port = (b[4] as u16) * 256 + (b[5] as u16);
+        let port = BigEndian::read_u16(&[b[4], b[5]]);
+
+        Peer { ip, port }
+    }
+
+    fn vec_from_bytes<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<Peer>, D::Error> {
+        d.deserialize_byte_buf(PeerVecVisitor)
+    }
+}
+
+impl <'de> Visitor<'de> for PeerVecVisitor {
+    type Value = Vec<Peer>;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("byte array")
+    }
+
+    fn visit_bytes<E: de::Error>(self, v: &[u8]) -> Result<Self::Value, E> {
+        Ok(v.chunks(6).map(Peer::from_bytes).collect())
+    }
+}
+
+pub fn request_peers(torrent: Torrent, peer_id: &String, port: &u16) -> Result<TrackerResponse, Box<dyn Error>> {
+    let url_hash = torrent.info_hash
+        .into_iter()
+        .map(percent_encode_byte)
+        .collect::<String>();
+    let base_url = format!("{}?info_hash={}", torrent.announce, url_hash);
+    let url = Url::parse_with_params(base_url.as_str(),
+                                     &[("peer_id", peer_id.to_string()),
+                                         ("port", port.to_string()),
+                                         ("uploaded", "0".to_string()),
+                                         ("downloaded", "0".to_string()),
+                                         ("compact", "1".to_string()),
+                                         ("left", torrent.length.to_string())])?;
+    let client = reqwest::blocking::Client::builder()
+        .timeout(Duration::from_secs(15))
+        .build()?;
+    let mut res = client.get(url)
+        .send()?;
+    let mut buf = Vec::new();
+
+    res.copy_to(&mut buf)?;
+
+    let tracker_response = serde_bencode::from_bytes::<TrackerResponse>(&buf.as_slice())?;
+
+    Ok(tracker_response)
+}
+
+
+#[cfg(test)]
+mod tests {
+
+}
