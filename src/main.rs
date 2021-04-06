b339162010bbda158989867e407f288c401657e2
Tabare Garcia Piniñor <tabareg14@gmail.com>
2021-04-06 10:11:53 -0300

    Move everything to run function

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,72 @@
 use std::env;
 use std::path::Path;
+use std::process::exit;
+use std::sync::Arc;
 use rand::Rng;
 use crate::torrent::Torrent;
+use crate::download_worker::{DownloaderWorker, TorrentState};
 use crate::connection::Connection;
-use std::net::{SocketAddr, IpAddr};
 
 mod connection;
 mod message;
 mod torrent;
 mod tracker_handler;
+mod download_worker;
+mod utils;
 
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
-    let path = Path::new(&args[1]);
-    let torrent = Torrent::open(path).unwrap();
+    let (torrent_path, out_path) = read_paths();
+
+    run(torrent_path, out_path);
+}
+
+fn run(torrent_path: String, out_path: Option<String>) {
+    const PORT: u16 = 6881;
+
     let peer_id = rand::thread_rng().gen::<[u8; 20]>().to_vec();
-    let port = 6881;
-    let mut res = tracker_handler::request_peers(&torrent, &peer_id, &port).unwrap();
+    let t_path = Path::new(&torrent_path);
+    let torrent = Torrent::open(t_path).unwrap();
+    let torrent_state = Arc::new(TorrentState::new(&torrent));
+    let res = tracker_handler::request_peers(&torrent, &peer_id, &PORT).unwrap();
+    let mut peer_queue = res.peers;
+    let mut workers = Vec::new();
 
-    let test_peer = res.peers.pop().unwrap();
+    println!("Torrent:\n{}",&torrent);
+    println!("Number of peers: {}", &peer_queue.len());
 
-    let peer_addr = SocketAddr::new(
-        IpAddr::from(test_peer.ip.to_owned()),
-        test_peer.port.to_owned()
-    );
-    let conn = Connection::connect(test_peer, torrent.info_hash, peer_id);
+    while workers.len() < TorrentState::MAX_CONCURRENT_PEERS as usize && peer_queue.len() > 0 {
+        let peer = peer_queue.pop().unwrap();
+        let torrent_state = Arc::clone(&torrent_state);
 
-    match conn {
-        Ok(mut conn) => {
-            let hs = conn.complete_handshake().unwrap();
-            let msg = message::Message::read(&conn.stream).unwrap();
+        match Connection::connect(peer, &torrent.info_hash, &peer_id) {
+            Ok(conn) => {
+                let handler = DownloaderWorker::new(torrent_state, conn).start();
 
-            println!("{:?}", msg);
-        },
-        Err(_) => println!("Connection with {} timed out.", peer_addr)
+                workers.push(handler);
+                println!("Total peers connected: {}", workers.len());
+            },
+            Err(_) => {
+                // println!("Could not connect to peer. Error: {}", e);
+                continue;
+            }
+        }
     }
 
-//    let hs = conn.complete_handshake().unwrap();
-//    let msg = message::Message::read(&conn.stream).unwrap();
-//
-//    println!("{:?}", msg);
+    for handler in workers {
+        handler.join().unwrap();
+    }
+}
+
+fn read_paths() -> (String, Option<String>) {
+    let args = env::args().collect::<Vec<String>>();
+    let torrent_path = args.get(1);
+    let out_path = args.get(2).cloned();
+
+    match torrent_path {
+        Some(path) => (path.to_owned(), out_path),
+        None => {
+            println!("Use bittorrent-client <torrent file> [out path]");
+            exit(0);
+        }
+    }
 }
